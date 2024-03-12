dc3d2b8c9996ecbd943fddf4ded450d64179724b
Anne <anne@procomponentes.com>
2024-03-12 13:46:48 +0100

    54. Rutas de tipo recurso vs tradicionales

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\Dashboard\TestController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,5 +15,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get("/", function(){
+    echo 'index';
     return view('welcome');
 });
+
+Route::resource('post', PostController::class);
+
+
+/* Estas son las rutas que genera el Route:resource:
+    Route::get('post', [PostController::class, 'index']);
+    Route::get('post/{post}', [PostController::class, 'show']);
+    Route::get('post/create', [PostController::class, 'create']);
+    Route::get('post/{post}/edit', [PostController::class, 'edit']);
+
+    Route::post('post/{post}/edit', [PostController::class, 'store']);
+
+    Route::put('post/{post}', [PostController::class, 'update']);
+
+    Route::delete('post/{post}', [PostController::class, 'destroy']);
+*/
